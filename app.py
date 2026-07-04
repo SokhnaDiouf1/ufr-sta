e920e9cd4b956c6a55a77ce017d019e79c0d8681
diouf.sokhna4@uam.edu.sn <sd1413413@gmail.com>
2026-07-05 00:38:17 +0200

    Quelques améliorations ont été faites

--- app.py
+++ app.py
@@ -14,5 +14,13 @@ def departements():
 def formations():
     return render_template('formations.html')
 
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/activites')
+def activites():
+    return render_template('activites.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
